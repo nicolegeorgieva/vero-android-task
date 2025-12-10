bad039798861bb10ead7d12d231d3020f1e0ef62
Nicole <nicole.georgievva@gmail.com>
2025-12-10 15:03:35 +0200

    Remove session on logout

--- app/src/main/java/com/example/app/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/app/ui/settings/SettingsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
 import com.example.app.common.ComposeViewModel
+import com.example.app.domain.SessionUseCase
 import com.example.app.navigation.Navigator
 import com.example.app.navigation.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,6 +16,7 @@ import javax.inject.Inject
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
   private val navigator: Navigator,
+  private val sessionUseCase: SessionUseCase,
 ) : ComposeViewModel<SettingsState, SettingsEvent>() {
   private var logoutButtonLoading by mutableStateOf(false)
 
@@ -41,9 +43,9 @@ class SettingsViewModel @Inject constructor(
   private fun handleLogoutClick() {
     viewModelScope.launch {
       logoutButtonLoading = true
+      sessionUseCase.logout()
       navigator.navigateTo(Screen.Login)
       logoutButtonLoading = false
-      // TODO
     }
   }
 }
