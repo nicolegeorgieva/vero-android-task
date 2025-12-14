ee2b42bac26f614d4ed26cfad02165ee63b0a630
Nicole <nicole.georgievva@gmail.com>
2025-12-14 19:43:39 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
   // region Navigation
   implementation(libs.androidx.navigation3.ui)
   implementation(libs.androidx.navigation3.runtime)
+  implementation(libs.androidx.lifecycle.viewmodel.navigation3)
   // endregion
 
   // region Hilt
