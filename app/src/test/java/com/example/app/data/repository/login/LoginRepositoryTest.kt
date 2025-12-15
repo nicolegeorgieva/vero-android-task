db580e3e14bf995c09dcb57ff294def609e65ac7
Nicole <nicole.georgievva@gmail.com>
2025-12-15 20:13:24 +0200

    Implement LoginRepositoryTest

--- app/src/test/java/com/example/app/data/repository/login/LoginRepositoryTest.kt
+++ app/src/test/java/com/example/app/data/repository/login/LoginRepositoryTest.kt
@@ -2,6 +2,8 @@ package com.example.app.data.repository.login
 
 import arrow.core.Either
 import com.example.app.data.ErrorMapper
+import com.example.app.data.ErrorResponse
+import com.example.app.data.datasource.login.IncorrectCredentialsException
 import com.example.app.data.datasource.login.LoginDataSource
 import com.example.app.data.datasource.login.OauthDto
 import com.example.app.data.datasource.login.SessionDto
@@ -13,6 +15,7 @@ import org.junit.Before
 import org.junit.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import java.net.UnknownHostException
 
 class LoginRepositoryTest {
   private lateinit var repository: LoginRepository
@@ -59,20 +62,56 @@ class LoginRepositoryTest {
     // then
     expectThat(res).isEqualTo(
       Either.Right(
-        Session(
-          accessToken = accessToken
-        )
+        Session(accessToken = accessToken)
       )
     )
   }
 
   @Test
-  fun `No internet error should be mapped to ErrorResponse-NoInternet`() {
-    TODO()
+  fun `No internet error should be mapped to ErrorResponse-NoInternet`() = runTest {
+    // given
+    coEvery {
+      dataSource.login(
+        username = rightCredentials.username,
+        password = rightCredentials.password,
+      )
+    } returns Either.Left(UnknownHostException())
+
+    // when
+    val res = repository.login(
+      username = rightCredentials.username,
+      password = rightCredentials.password,
+    )
+
+    // then
+    expectThat(res).isEqualTo(
+      Either.Left(LoginError.Other(ErrorResponse.NoInternet))
+    )
   }
 
   @Test
-  fun `Wrong credentials should lead to LoginError-IncorrectCredentials`() {
-    TODO()
+  fun `Wrong credentials should lead to LoginError-IncorrectCredentials`() = runTest {
+    // given
+    val wrongCredentials = rightCredentials.copy(
+      username = "${rightCredentials.username}1",
+      password = "${rightCredentials.password}88"
+    )
+    coEvery {
+      dataSource.login(
+        username = wrongCredentials.username,
+        password = wrongCredentials.password,
+      )
+    } returns Either.Left(IncorrectCredentialsException())
+
+    // when
+    val res = repository.login(
+      username = wrongCredentials.username,
+      password = wrongCredentials.password,
+    )
+
+    // then
+    expectThat(res).isEqualTo(
+      Either.Left(LoginError.IncorrectCredentials)
+    )
   }
 }
