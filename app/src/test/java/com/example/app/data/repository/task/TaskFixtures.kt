76d8f1d0aee611aaad47e684bb9b7fe709383456
Nicole <nicole.georgievva@gmail.com>
2025-12-17 15:47:09 +0200

    Create TaskFixtures

---
+++ app/src/test/java/com/example/app/data/repository/task/TaskFixtures.kt
@@ -0,0 +1,23 @@
+package com.example.app.data.repository.task
+
+import com.example.app.data.datasource.task.TaskDto
+
+private val taskMapper = TaskMapper()
+
+val TASK_1_DTO = TaskDto(
+  task = "1",
+  title = "Buy materials",
+  description = "From local market",
+  colorCode = ""
+)
+val TASK_1 = taskMapper.dtoToDomain(TASK_1_DTO)
+val TASK_1_ENTITY = taskMapper.domainToEntity(TASK_1)
+
+val TASK_2_DTO = TaskDto(
+  task = "2",
+  title = "Build the base",
+  description = "",
+  colorCode = ""
+)
+val TASK_2 = taskMapper.dtoToDomain(TASK_2_DTO)
+val TASK_2_ENTITY = taskMapper.domainToEntity(TASK_2)
--- app/src/test/java/com/example/app/data/repository/task/TaskRepositoryTest.kt
+++ app/src/test/java/com/example/app/data/repository/task/TaskRepositoryTest.kt
@@ -3,7 +3,6 @@ package com.example.app.data.repository.task
 import app.cash.turbine.test
 import arrow.core.Either
 import com.example.app.data.ErrorMapper
-import com.example.app.data.datasource.task.TaskDto
 import com.example.app.data.datasource.task.TaskLocalDataSource
 import com.example.app.data.datasource.task.TaskRemoteDataSource
 import io.mockk.coEvery
@@ -16,22 +15,13 @@ import org.junit.Test
 import strikt.api.expectThat
 import strikt.arrow.isRight
 import strikt.assertions.isNotNull
+import java.net.UnknownHostException
 
 class TaskRepositoryTest {
-  private val taskMapper = TaskMapper()
   private val localDataSource = mockk<TaskLocalDataSource>()
   private val remoteDataSource = mockk<TaskRemoteDataSource>()
 
   private lateinit var repository: TaskRepository
-
-  private val taskDto = TaskDto(
-    task = "1",
-    title = "Buy materials",
-    description = "From local market",
-    colorCode = ""
-  )
-  private val task = taskMapper.dtoToDomain(taskDto)
-  private val taskEntity = taskMapper.domainToEntity(task)
 
   @Before
   fun setup() {
@@ -44,26 +34,44 @@ class TaskRepositoryTest {
   }
 
   @Test
-  fun `Tasks from DB and successful fetch of tasks from server`() = runTest {
+  fun `Tasks from DB & successful fetch of tasks from server`() = runTest {
     // given
     coEvery {
       localDataSource.getAllTasks()
-    } returns listOf(taskEntity)
+    } returns listOf(TASK_1_ENTITY)
     coEvery {
       remoteDataSource.fetchTasks()
-    } returns Either.Right(listOf(taskDto, taskDto))
+    } returns Either.Right(listOf(TASK_1_DTO, TASK_2_DTO))
     coEvery {
-      localDataSource.insertAllTasks(task = listOf(taskEntity, taskEntity))
+      localDataSource.insertAllTasks(task = listOf(TASK_1_ENTITY, TASK_2_ENTITY))
     } just runs
 
     // when
     repository.getTasks(this).test {
       // then
       val first = awaitItem()
-      expectThat(first).isNotNull().isRight(listOf(task))
+      expectThat(first).isNotNull().isRight(listOf(TASK_1))
 
       val second = awaitItem()
-      expectThat(second).isNotNull().isRight(listOf(task, task))
+      expectThat(second).isNotNull().isRight(listOf(TASK_1, TASK_2))
+    }
+  }
+
+  @Test
+  fun `Tasks from DB & no internet`() = runTest {
+    // given
+    coEvery {
+      localDataSource.getAllTasks()
+    } returns listOf(TASK_1_ENTITY)
+    coEvery {
+      remoteDataSource.fetchTasks()
+    } returns Either.Left(UnknownHostException())
+
+    // when
+    repository.getTasks(this).test {
+      // then
+      val data = awaitItem()
+      expectThat(data).isNotNull().isRight(listOf(TASK_1))
     }
   }
 }
