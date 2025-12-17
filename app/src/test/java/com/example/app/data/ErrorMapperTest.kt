ca8e3b5945c3e602f2e66e6ec9a192bb72d29490
Nicole <nicole.georgievva@gmail.com>
2025-12-17 17:27:31 +0200

    Improve ErrorMapperTest

--- app/src/test/java/com/example/app/data/ErrorMapperTest.kt
+++ app/src/test/java/com/example/app/data/ErrorMapperTest.kt
@@ -1,27 +1,39 @@
 package com.example.app.data
 
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
+import org.junit.runner.RunWith
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 import java.net.UnknownHostException
 
+@RunWith(TestParameterInjector::class)
 class ErrorMapperTest {
   private val errorMapper = ErrorMapper()
 
-  @Test
-  fun `Map no internet error`() = runTest {
-    // when
-    val error = errorMapper.map(UnknownHostException())
-    // then
-    expectThat(error).isEqualTo(ErrorResponse.NoInternet)
+  enum class ErrorMappingTestCase(
+    val error: Throwable,
+    val expected: ErrorResponse,
+  ) {
+    NO_INTERNET(
+      error = UnknownHostException(),
+      expected = ErrorResponse.NoInternet
+    ),
+    OTHER(
+      error = Exception("error"),
+      expected = ErrorResponse.Other
+    )
   }
 
   @Test
-  fun `Map server error`() = runTest {
+  fun `Error mapping`(
+    @TestParameter testCase: ErrorMappingTestCase,
+  ) = runTest {
     // when
-    val error = errorMapper.map(Exception("error"))
+    val error = errorMapper.map(testCase.error)
     // then
-    expectThat(error).isEqualTo(ErrorResponse.Other)
+    expectThat(error).isEqualTo(testCase.expected)
   }
 }
