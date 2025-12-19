c02c18e84dcccbd7c78fdc7323bd38f07e3de09f
Nicole <nicole.georgievva@gmail.com>
2025-12-19 13:40:53 +0200

    Fix & improve TaskMapperTest

--- app/src/test/java/com/example/app/data/datastore/SessionFixtures.kt
+++ app/src/test/java/com/example/app/data/datastore/SessionFixtures.kt
@@ -2,5 +2,5 @@ package com.example.app.data.datastore
 
 import com.example.app.domain.model.Session
 
-val ACCESS_TOKEN_1 = "test123"
+const val ACCESS_TOKEN_1 = "test123"
 val SESSION_1 = Session(ACCESS_TOKEN_1)
--- app/src/test/java/com/example/app/data/repository/task/TaskMapperTest.kt
+++ app/src/test/java/com/example/app/data/repository/task/TaskMapperTest.kt
@@ -1,5 +1,8 @@
 package com.example.app.data.repository.task
 
+import com.example.app.data.database.task.TaskEntity
+import com.example.app.data.datasource.task.TaskDto
+import com.example.app.domain.model.Task
 import org.junit.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
@@ -10,30 +13,82 @@ class TaskMapperTest {
   // region entityToDomain()
   @Test
   fun `Entity to domain`() {
+    // Given
+    val entity = TaskEntity(
+      id = "1",
+      title = "Buy materials",
+      description = "From local market",
+      colorCode = null
+    )
     // when
-    val task = taskMapper.entityToDomain(TASK_1_ENTITY)
+    val task = taskMapper.entityToDomain(entity)
     // then
-    expectThat(task).isEqualTo(TASK_1)
+    expectThat(task).isEqualTo(
+      Task(
+        id = "1",
+        title = "Buy materials",
+        description = "From local market",
+        colorHex = null
+      )
+    )
   }
   // endregion
 
   // region dtoToDomain()
   @Test
-  fun `DTO to domain`() {
+  fun `DTO to domain with empty colorCode`() {
+    // given
+    val dto = TaskDto(
+      task = "1",
+      title = "Buy materials",
+      description = "From local market",
+      colorCode = ""
+    )
     // when
-    val task = taskMapper.dtoToDomain(TASK_1_DTO)
+    val task = taskMapper.dtoToDomain(dto)
     // then
-    expectThat(task).isEqualTo(TASK_1)
+    expectThat(task).isEqualTo(
+      Task(
+        id = "1",
+        title = "Buy materials",
+        description = "From local market",
+        colorHex = null
+      )
+    )
+  }
+
+  @Test
+  fun `DTO to domain with colorCode`() {
+    // given
+    val dto = TASK_1_DTO.copy(colorCode = "#FFFFFF")
+    // when
+    val task = taskMapper.dtoToDomain(dto)
+    // then
+    expectThat(task.colorHex).isEqualTo("#FFFFFF")
   }
   // endregion
 
   // region domainToEntity()
   @Test
   fun `Domain to entity`() {
+    // given
+    val task = Task(
+      id = "1",
+      title = "Buy materials",
+      description = "From local market",
+      colorHex = null
+    )
     // when
-    val entity = taskMapper.domainToEntity(TASK_1)
+    val entity = taskMapper.domainToEntity(task)
     // then
-    expectThat(entity).isEqualTo(TASK_1_ENTITY)
+    expectThat(entity).isEqualTo(
+      TaskEntity(
+        id = "1",
+        title = "Buy materials",
+        description = "From local market",
+        colorCode = null
+      )
+    )
   }
   // endregion
 }
